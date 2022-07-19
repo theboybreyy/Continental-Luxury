d823f26e28251ce316523f1e6827656e2bb90ba3
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-19 16:27:33 +0000

    Concatenating Paths

--- Reference/path_demo.js
+++ Reference/path_demo.js
@@ -7,4 +7,12 @@ console.log(path.basename(__filename));
 // Directory Name
 console.log(path.dirname(__filename));
 
-// 
+// File Extension
+console.log(path.extname(__filename));
+
+// Create Path Object 
+console.log(path.parse(__filename).base);
+
+// Concatenate Paths
+console.log(path.join(__dirname, 'test', 'hello.html'));
+ 
