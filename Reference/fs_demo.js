c0c2b1de24fb4dcf79b045c63d4b02995d6e3af6
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-19 16:52:40 +0000

    Recreating Script.ex  File

--- Reference/fs_demo.js
+++ Reference/fs_demo.js
@@ -8,13 +8,20 @@ const path = require('path');
 // });
 
 // Create and Write to File 
-fs.writeFile(path.join(__dirname, '/test', 'hello.py'), 'Hello World, this is a Python File!', err => {
-    if (err) throw console.warn(err);
-    console.log('File written to...')
-});
+fs.writeFile(
+    path.join(__dirname, '/test', 'hello.py'), 'Hello World, this is a Python File!', err => {
+        if (err) throw console.warn(err);
+        console.log('File written to...');
 
 
-fs.writeFile(path.join(__dirname, '/test', 'script.ex'), 'Hello World, this is a Python File!', err => {
-    if (err) throw console.warn(err);
-    console.log('File written to...')
-});
+        // File Append
+        fs.appendFile(
+            path.join(__dirname, '/test', 'script.ex'),
+            'I love Node JS',
+            err => {
+                if (err) throw console.warn(err);
+                console.log('File written to...')
+            });
+    });
+
+
