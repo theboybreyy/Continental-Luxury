c425022d2b677b607da74f0775a45128a5120bbc
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-19 16:41:01 +0000

    Creating New Files

--- Reference/fs_demo.js
+++ Reference/fs_demo.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 // Create Folder
-fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
+// fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
+//     if (err) throw console.warn(err);
+//     console.log('Folder Created Successfully')
+// });
+
+// Create and Write to File 
+fs.writeFile(path.join(__dirname, '/test', 'hello.py'), 'Hello World, this is a Python File!', err => {
     if (err) throw console.warn(err);
-    console.log('Folder Created Successfully')
+    console.log('File written to...')
 });
