2bb4a4cbc848305c11ec09d2c8dec36ff079d4a8
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-19 16:59:06 +0000

    Rename a file

--- Reference/fs_demo.js
+++ Reference/fs_demo.js
@@ -8,20 +8,34 @@ const path = require('path');
 // });
 
 // Create and Write to File 
-fs.writeFile(
-    path.join(__dirname, '/test', 'hello.py'), 'Hello World, this is a Python File!', err => {
+// fs.writeFile(
+//     path.join(__dirname, '/test', 'hello.py'), 'Hello World, this is a Python File!', err => {
+//         if (err) throw console.warn(err);
+//         console.log('File written to...');
+
+
+//         // File Append
+//         fs.appendFile(
+//             path.join(__dirname, '/test', 'script.ex'),
+//             'I love Node JS',
+//             err => {
+//                 if (err) throw console.warn(err);
+//                 console.log('File written to...')
+//             });
+//     });
+
+
+
+// Read File
+fs.readFile(path.join(__dirname, '/test', 'hello.py'), 'utf-8', (err, data) => {
         if (err) throw console.warn(err);
-        console.log('File written to...');
-
-
-        // File Append
-        fs.appendFile(
-            path.join(__dirname, '/test', 'script.ex'),
-            'I love Node JS',
-            err => {
-                if (err) throw console.warn(err);
-                console.log('File written to...')
-            });
+        console.log(data);
     });
 
 
+
+// Rename A File
+fs.rename(path.join(__dirname, '/test', 'hello.py'), 'utf-8', (err, data) => {
+    if (err) throw console.warn(err);
+    console.log(data);
+});
