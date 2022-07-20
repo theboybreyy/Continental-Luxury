e62a0b57c6a95b556a894830d49d57b01c0a2d1f
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-20 09:50:38 +0000

    OS Platform

--- Reference/os_demo.js
+++ Reference/os_demo.js
@@ -1,19 +1,4 @@
-const fs = require('fs');
-const path = require('path');
+const os = require ('os');
 
-// Create and Write to File 
-fs.writeFile(
-    path.join(__dirname, '/test', 'osm_demo.js'), 'This is an OS Module File.', err => {
-        if (err) throw console.warn(err);
-        console.log('File written to...');
-
-
-        // // File Append
-        // fs.appendFile(
-        //     path.join(__dirname, '/Reference', 'script.ex'),
-        //     'I love Node JS',
-        //     err => {
-        //         if (err) throw console.warn(err);
-        //         console.log('File written to...')
-        //     });
-    });
+// Platform
+console.log(os.platform());
