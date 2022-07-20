5420b37f00ee62d570292f3db07296b9e42724c9
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-20 22:13:20 +0000

    Serving HTML contents in the window

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const  http = require('http');
+const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url == '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Welcome Everyone!</h1>')
-    }
+    if (req.url == '/') {
+        fs.readFile(path.join(__dirname, 'Public', 'index.html'), (err, content) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
 
-});
+        })
+    }
+    if (req.url == '/about') {
+        fs.readFile(path.join(__dirname, 'Public', 'about.html'), (err, content) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+
+        })
+    }
+})
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log('Server running on port ' + PORT ));
+server.listen(PORT, () => console.log('Server running on port ' + PORT));
