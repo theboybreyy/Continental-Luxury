363902961945644b331d96d70c89e2f0b1f795b7
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-20 09:40:26 +0000

    File Created with FS Module

--- Reference/os_demo.js
+++ Reference/os_demo.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
 // Create and Write to File 
 fs.writeFile(
-    path.join(__dirname, '/Reference', 'osm_demo.js'), 'This is an OS Module File.', err => {
+    path.join(__dirname, '/test', 'osm_demo.js'), 'This is an OS Module File.', err => {
         if (err) throw console.warn(err);
         console.log('File written to...');
 
