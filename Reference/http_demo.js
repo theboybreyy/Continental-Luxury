51fd029efc49ab1b3d24ce3d2b8a31fea3066199
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-20 21:25:20 +0000

    Updated the http object

--- Reference/http_demo.js
+++ Reference/http_demo.js
@@ -1,3 +1,8 @@
 const http = require('http');
 
 // Create Server Object
+http.createServer((req, res) => {
+    // Write Respone
+    res.write('Hello World');
+    res.end()
+}).listen(5000, () => console.log('Server running....'))
