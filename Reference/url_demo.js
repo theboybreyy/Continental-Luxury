07989a08b72f60b4a5fabdd65496fe8a0f109417
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-20 10:17:48 +0000

    Added Port Number

--- Reference/url_demo.js
+++ Reference/url_demo.js
@@ -1,6 +1,6 @@
 const url = require('url');
 
-const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+const myUrl = new URL('http://mywebsite.com:8000/hello.html?id=100&status=active');
 
 // Serialized URL
 console.log(myUrl.href);
@@ -8,3 +8,6 @@ console.log(myUrl.toString());
 
 // Host Root
 console.log(myUrl.host)
+
+// Hostname
+console.log(myUrl.hostname)
