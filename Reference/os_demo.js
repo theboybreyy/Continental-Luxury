18f6e25c0c88df74ddf653aa80b4b5ba014ce92a
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-20 09:37:14 +0000

    Creating a file for the OS Module

--- Reference/os_demo.js
+++ Reference/os_demo.js
@@ -1,16 +1,16 @@
 // Create and Write to File 
 fs.writeFile(
-    path.join(__dirname, '/test', 'hello.py'), 'Hello World, this is a Python File!', err => {
+    path.join(__dirname, '/Reference', 'osm_demo.js'), 'This is an OS Module File.', err => {
         if (err) throw console.warn(err);
         console.log('File written to...');
 
 
-        // File Append
-        fs.appendFile(
-            path.join(__dirname, '/test', 'script.ex'),
-            'I love Node JS',
-            err => {
-                if (err) throw console.warn(err);
-                console.log('File written to...')
-            });
+        // // File Append
+        // fs.appendFile(
+        //     path.join(__dirname, '/Reference', 'script.ex'),
+        //     'I love Node JS',
+        //     err => {
+        //         if (err) throw console.warn(err);
+        //         console.log('File written to...')
+        //     });
     });
