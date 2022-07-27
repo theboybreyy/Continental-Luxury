bb8a04276c7d6e70cb35af4d37d6d8c310485f92
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-27 14:53:20 +0000

    Updating the 404 Page

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ const server = http.createServer((req, res) => {
 
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log('Server running on port ' + PORT));
+server.listen(PORT, () => console.log('Server running on Port ' + PORT + '...'));
