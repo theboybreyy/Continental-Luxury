a1f114d342b40f0f49fcf7930c43a34b77c1b204
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-29 19:38:06 +0000

    Added the back-call for the data

--- Public/img/data.sql
+++ Public/img/data.sql
@@ -3,3 +3,13 @@ class Customer(db.Entity):
 
 class Order(db.Entity):
     customer = Required(Customer)
+
+    class Customer(db.Entity):
+    orders = Set('Order')
+
+class Order(db.Entity):
+    customer = Required(Customer)class Customer(db.Entity):
+    orders = Set('Order')
+
+class Order(db.Entity):
+    customer = Required(Customer)
